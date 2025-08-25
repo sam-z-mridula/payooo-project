9630bf2db11203076bea6287586f4b3d4ab69038
sam-z-mridula <sameehamridula@gmail.com>
2025-08-25 22:06:43 +0600

    added functional withdraw money section with toggling feature

--- home.js
+++ home.js
@@ -28,5 +28,51 @@ document.getElementById("add-btn").addEventListener("click", function (e) {
   const newAmount = availableAmount + addAmount;
 
   document.getElementById("available-amount").innerText = newAmount;
-  alert("Money successfully added to the main balance");
+  alert("Activity Successful");
 });
+
+/* ---- Toggling ---- */
+document.getElementById("add-money-btn").addEventListener("click", function(){
+    document.getElementById("cashout-parent").style.display = "none";
+    document.getElementById("notice").style.display = "none";
+    document.getElementById("add-money-parent").style.display = "block";
+})
+document.getElementById("cashout-btn").addEventListener("click", function(){
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("notice").style.display = "none";
+    document.getElementById("cashout-parent").style.display = "block";
+})
+
+/*------------------ Cash Out Functionality ------------------ */
+document.getElementById("withdraw-amount").addEventListener("keyup", function(){
+    const wdAmount = parseInt(document.getElementById("withdraw-amount").value);
+    const wdBtn = document.getElementById("withdraw-btn");
+    if (wdAmount > 0) {
+        wdBtn.removeAttribute("disabled");
+    } else {
+        wdBtn.setAttribute("disabled", true);
+    }
+})
+
+document.getElementById("withdraw-btn").addEventListener("click", function(e){
+    e.preventDefault();
+    const agentNum = 88012345678;
+    const wdPinNum = 1234;
+
+    const userAgentNum = parseInt(document.getElementById("agent-number").value);
+    const userWdPin = parseInt(document.getElementById("wd-pin").value);
+
+    if (agentNum !== userAgentNum || wdPinNum !== userWdPin) {
+    alert("⚠Invalid Credential⚠");
+    return;
+    }
+
+    const wdAmount = parseInt(document.getElementById("withdraw-amount").value);
+    const availableAmount = parseInt(
+    document.getElementById("available-amount").innerText
+  );
+  const newAmount = availableAmount - wdAmount;
+
+  document.getElementById("available-amount").innerText = newAmount;
+  alert("Activity Successful");
+})

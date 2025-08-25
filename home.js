7b73428ff26806fc59443af8fea0c91ced5eabd3
sam-z-mridula <sameehamridula@gmail.com>
2025-08-25 19:08:30 +0600

    enhanced the functionality of add money section

--- home.js
+++ home.js
@@ -1,26 +1,32 @@
-document.getElementById("add-btn").addEventListener("click", function(e){
-    e.preventDefault();
-    const bankAcc = 88012345678;
-    const pinNum = 1234;
-
-    const userBankAcc = parseInt(document.getElementById("bank-account").value);
-    const userPin = parseInt(document.getElementById("pin").value);
-    
-    if (bankAcc !== userBankAcc || pinNum !== userPin) {
-        alert("Invalid Credential");
-        return;
-    }
-
+document.getElementById("add-amount").addEventListener("keyup", function(){
     const addAmount = parseInt(document.getElementById("add-amount").value);
-
-    if (addAmount <= 0) {
-        alert("Invalid Amount");
-        return;
+    const addBtn = document.getElementById("add-btn");
+    if (addAmount > 0) {
+        addBtn.removeAttribute("disabled");
+    } else {
+        addBtn.setAttribute("disabled", true);
     }
-
-    const availableAmount = parseInt(document.getElementById("available-amount").innerText);
-    const newAmount = availableAmount + addAmount;
-
-    document.getElementById("available-amount").innerText = newAmount;
-    alert("Money successfully added to the main balance");
 })
+
+document.getElementById("add-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const bankAcc = 88012345678;
+  const pinNum = 1234;
+
+  const userBankAcc = parseInt(document.getElementById("bank-account").value);
+  const userPin = parseInt(document.getElementById("pin").value);
+
+  if (bankAcc !== userBankAcc || pinNum !== userPin) {
+    alert("⚠Invalid Credential⚠");
+    return;
+  }
+
+  const addAmount = parseInt(document.getElementById("add-amount").value);
+  const availableAmount = parseInt(
+    document.getElementById("available-amount").innerText
+  );
+  const newAmount = availableAmount + addAmount;
+
+  document.getElementById("available-amount").innerText = newAmount;
+  alert("Money successfully added to the main balance");
+});
